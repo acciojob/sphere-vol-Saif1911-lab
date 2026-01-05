601111bdcc82e296645de13ceb5ad58f50898570
Mohammed Saif <mohdsaifwork19@gmail.com>
2026-01-05 12:52:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-function volume_sphere() {
-    //Write your code here
-  
-} 
+function volume_sphere(e) {
+    e.preventDefault();
+
+    const radius = Number(e.target.radius.value);
+    const volumeEle = e.target.volume;
+	if(isNaN(radius) || radius<=0){
+		volumeEle.value = 'NaN'
+	}
+
+    const result = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    volumeEle.value = result.toFixed(4);
+}
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
